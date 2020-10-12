1baa8550abae4cccf774a553fcf3813288385f37
destro <safdari07@outlook.com>
2020-10-12 05:22:41 +0100

    split temp functions into another module

---
+++ src/temp.js
@@ -0,0 +1,11 @@
+//scripts for temp conversion.
+
+export default function kelvintocelsius(kel) { 
+    let c = Math.round(kel - 273);
+    return c;
+}
+
+export function kelvintofahrenheit(kel) { 
+    let f = (kel - 273.15) * (9/5) + 32;
+    return f;
+}
