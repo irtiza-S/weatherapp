14465a789c4434be5db5ab7dfa14e2aabdc77289
irtiza-S <61486852+irtiza-S@users.noreply.github.com>
2021-09-13 01:52:17 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 //declare variables + import other modules
 let inptfield = document.getElementById('location');
-let apiKey = '&APPID=2c21a0893903fb1661f16debd471f221';
+let apiKey = 'apikey';
 let apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
 const btn = document.getElementById('btn')
 let gif = document.getElementById('gif')
